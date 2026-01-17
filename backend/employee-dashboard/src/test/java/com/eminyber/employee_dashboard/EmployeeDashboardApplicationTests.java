57bdbe77bc2ae444e7286e397388dd56f7f8d58e
Emil Nyberg <nyberg.emil01@gmail.com>
2026-01-17 14:05:56 +0100

    Initial commit. Creted product structure and the Spring Boot application

---
+++ backend/employee-dashboard/src/test/java/com/eminyber/employee_dashboard/EmployeeDashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eminyber.employee_dashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeDashboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
